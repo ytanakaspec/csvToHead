031b95ef5f91bd7f58a56b8e7956cce31233eb89
ytanakaspec <ytanaka@spec.jp>
2020-04-08 11:59:22 +0900

    xlsxからスプレッドシートの見出しを作るように変更

--- opt/make_head.py
+++ opt/make_head.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import csv
+import itertools
 
 # ファイル名を設定する
-file_name = 'ｻﾝﾌﾟﾙﾃﾞｰﾀ'
+file_name = 'sampleHead'
 
-df = pd.read_excel(f'./{file_name}.xlsx')
+df = pd.read_excel(f'./{file_name}.xlsx', header=None, index_col=None)
 
 df_arr = df.values.tolist()
+df_arr = list(itertools.chain.from_iterable(df_arr))
 
-print(df_arr)
+#print(df_arr)
+
+head_list = list(map(lambda x: str(df_arr.index(x)) + ": { text: '" + x + "' }", df_arr))
+
+print("******************************************")
+print(head_list)
